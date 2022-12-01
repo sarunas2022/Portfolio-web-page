7d328d48a920fbbbb14a9497cc9d3d20359f98e4
Šarūnas Grabauskas <sarunas.grabauskas@gmail.com>
2022-12-01 16:03:20 +0200

    added working contact section, with auto replays

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.scss';
 import Header from './components/header/Header';
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/footer/Footer';
-import Skills from './components/sections/Skills';
+import Skills from './components/sections/skills/Skills';
+import Contacts from './components/sections/contacts/Contacts';
 
 function App() {
     return (
@@ -12,7 +13,7 @@ function App() {
             <section id='about'></section>
             <Skills></Skills>
             <section id='projects'></section>
-            <section id='contacts'></section>
+            <Contacts />
             <Footer />
         </div>
     );
