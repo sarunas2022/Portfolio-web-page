5076eab7f8cae11ce3753fe51de4da7e2c44074f
Šarūnas Grabauskas <sarunas.grabauskas@gmail.com>
2022-12-07 16:25:12 +0200

    added projects section

--- src/components/sections/about/About.jsx
+++ src/components/sections/about/About.jsx
@@ -13,14 +13,23 @@ export default function About() {
 
                 <div className={styles.aboutMe}>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit. Similique ut, nemo facilis nulla neque modi
-                        dignissimos illum ipsum excepturi praesentium nisi
-                        aliquam omnis magni molestiae veritatis voluptas quia
-                        nihil commodi ab, officiis sit aperiam aut obcaecati?
-                        Optio nisi repudiandae dolore ipsum, repellendus nam
-                        adipisci laboriosam possimus beatae quibusdam quia
-                        exercitationem.
+                        If I would have to describe myself in one sentence, I
+                        would say that I am very easy to get along with, funny
+                        and always at the center of everything.
+                    </p>
+                    <p>
+                        At the end of 2021, I took a leap and started to learn
+                        modern app development at Code Academy. I enjoy coding -
+                        constantly facing problems and finding solutions become
+                        an excellent motivation for me - it seems like a
+                        never-ending process that drives me forward to improve
+                        every day.
+                    </p>
+                    <p>
+                        When I'm not coding, I'm usually playing video games,
+                        testing out a new recipe, watching basketball, spending
+                        time with friends and family and traveling whenever I
+                        can.
                     </p>
                 </div>
             </div>
