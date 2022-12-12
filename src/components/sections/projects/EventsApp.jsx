4e5d9a9933bb0454669f7f1d216e6f0735debb68
Šarūnas Grabauskas <sarunas.grabauskas@gmail.com>
2022-12-12 16:49:37 +0200

    event app was added to projects

---
+++ src/components/sections/projects/EventsApp.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import styles from './Projects.module.scss';
+import projectImage from './../../../files/projectsImages/eventsApp/events.png';
+
+export default function EventsApp() {
+    return (
+        <div className={styles.project}>
+            <div className={styles.textWrapper}>
+                <div className={styles.description} n>
+                    <h2> Event register</h2>
+                    <div className={styles.technologies}>
+                        <p>SASS</p>
+                        <p>MUI</p>
+                        <p>React</p>
+                        <p>NodeJS</p>
+                        <p>MySQL</p>
+                        <p>ExpressJS</p>
+                    </div>
+
+                    <p>
+                        This app was my final projects for Code Academy course.
+                        It consists of 3 parts - database, server and client.
+                        The purpose of the app is to allow companies engaged in
+                        the organization of events to register new events and
+                        add/edit/delete guest of particular event.
+                    </p>
+                    <p>
+                        All CRUD operations where used and data is saved in
+                        database. It also has registration and login page with
+                        back-end authentication/authorization in server side
+                        using JWT token, passwords are saved in database hashed
+                        with BCRIPT, form validation using JOI and for routing
+                        EXPRESS npm package was used.
+                    </p>
+                    <div className={styles.projectButtons}>
+                        <a
+                            target='_blank'
+                            rel='noreferrer'
+                            href='https://github.com/sarunas2022/React-app-Events'
+                        >
+                            <button>View source code</button>
+                        </a>
+                        <a target='_blank' rel='noreferrer' href='#'>
+                            <button>Visit the website</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div className={styles.visualization}>
+                <img src={projectImage} alt='img' />
+            </div>
+        </div>
+    );
+}
