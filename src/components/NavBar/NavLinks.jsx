5f506f750a0e706b2c93a64a3bdc7c0e23263f1a
Šarūnas Grabauskas <sarunas.grabauskas@gmail.com>
2022-12-13 17:42:10 +0200

    ddd

--- src/components/NavBar/NavLinks.jsx
+++ src/components/NavBar/NavLinks.jsx
@@ -1,24 +1,57 @@
 import React from 'react';
 import styles from './NavBar.module.scss';
+import { motion } from 'framer-motion';
 
 export default function NavLinks(props) {
+    // variables for li items animation using framer-motion
+    const animateFrom = { opacity: 0, y: -40 };
+    const animateTo = { opacity: 1, y: 0 };
     return (
-        <ul className={styles.links}>
-            <li onClick={() => props.isMobile && props.closeMobileMenu()}>
+        <motion.ul
+            className={styles.links}
+            initial={{ x: -40 }}
+            animate={{ x: 0 }}
+        >
+            <motion.li
+                initial={animateFrom}
+                animate={animateTo}
+                transition={{ delay: 0.05 }}
+                onClick={() => props.isMobile && props.closeMobileMenu()}
+            >
                 <a href='#home'>Home</a>
-            </li>
-            <li onClick={() => props.isMobile && props.closeMobileMenu()}>
+            </motion.li>
+            <motion.li
+                initial={animateFrom}
+                animate={animateTo}
+                transition={{ delay: 0.1 }}
+                onClick={() => props.isMobile && props.closeMobileMenu()}
+            >
                 <a href='#skills'>Skills</a>
-            </li>
-            <li onClick={() => props.isMobile && props.closeMobileMenu()}>
+            </motion.li>
+            <motion.li
+                initial={animateFrom}
+                animate={animateTo}
+                transition={{ delay: 0.2 }}
+                onClick={() => props.isMobile && props.closeMobileMenu()}
+            >
                 <a href='#projects'>Latest work</a>
-            </li>
-            <li onClick={() => props.isMobile && props.closeMobileMenu()}>
+            </motion.li>
+            <motion.li
+                initial={animateFrom}
+                animate={animateTo}
+                transition={{ delay: 0.3 }}
+                onClick={() => props.isMobile && props.closeMobileMenu()}
+            >
                 <a href='#about'>About me</a>
-            </li>
-            <li onClick={() => props.isMobile && props.closeMobileMenu()}>
+            </motion.li>
+            <motion.li
+                initial={animateFrom}
+                animate={animateTo}
+                transition={{ delay: 0.4 }}
+                onClick={() => props.isMobile && props.closeMobileMenu()}
+            >
                 <a href='#contacts'>Contact me</a>
-            </li>
-        </ul>
+            </motion.li>
+        </motion.ul>
     );
 }
